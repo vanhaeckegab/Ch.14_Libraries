ab0647efcdfa5517cfb95e1ef2cd632157fa8831
Marc Hermon <hermonm@urbandaleschools.com>
2021-04-08 06:41:33 -0500

    Update 14.0_Jedi_Training.py

--- 14.0_Jedi_Training.py
+++ 14.0_Jedi_Training.py
@@ -1,8 +1,8 @@
 '''
 Sign your name:________________
  
-For this test, take your 30 box program and remove the Ball Class into a seperate file called Box_Builder.py. Now just
-import the Ball Class into your main program. No need to pull request this. Just show it to your instructor if you can get
+For this test, take your 30 box program and remove the Box Class into a seperate file called Box_Builder.py. Now just
+import the Box Class into your main program. No need to pull request this. Just show it to your instructor if you can get
 it to work. Use if __name__ =="__main__":
 
 '''
